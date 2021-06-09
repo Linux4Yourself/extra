ea17151710252cf31eabf331d3d5f7d2809e820a
Anton Maisak <anton@maisak.ru>
2021-06-09 10:43:50 +0300

    pkgs fix

--- docs/common.js
+++ docs/common.js
@@ -1,31 +1,64 @@
-Vue.component('package-info', {
+Vue.component('pkg', {
 	props: {
-		package: Object,
+		name: String,
 		showsbu: Boolean,
+		instsize: Boolean,
+	},
+	data() {
+		return {
+			package: {},
+		};
+	},
+	mounted() {
+		getPackage(this.name)
+			.then(res => (this.package = res));
 	},
 	template: `
-	<div class="pkg">
+	<div v-if="package" class="pkg">
 		<p>{{ package.description }}</p>
 		<p class="pkg-desc">
-			Ссылка для скачивания: <a :href="package.url"><b>{{ package.url }}</b></a>
+			Ссылка для скачивания: <a :href="package.url"><b>{{ package.downloadUrl }}</b></a>
 			<br />
 			Текущая версия: <b>{{ package.version }}</b>
 			<br />
 			Домашняя страница: <a :href="package.homeUrl"><b>{{ package.homeUrl }}</b></a>
 			<br />
 			Размер архива: <b>{{ package.size }} Mb</b>
-
 			<span v-if="showsbu">
 				<br />
 				<span v-if="package.sbu">Приблизительное время сборки: <b>{{ package.sbu }} SBU</b></span>
+			</span>
+			<span v-if="instsize">
+				<br />
+				<span v-if="package.installedSize">Приблизительный размер после установки: <b>{{ package.installedSize }} МБ</b></span>
 			</span>
 		</p>
 		<slot></slot>
 	</div>`,
 });
 
+Vue.component('package-script', {
+	props: {
+		package: String,
+		type: String,
+	},
+	data() {
+		return {
+			info: null
+		};
+	},
+	mounted() {
+		getPackageScript(this.package, this.type)
+			.then(res => (this.info = res));
+	},
+	template: `
+<pre class="pre">
+{{ info }}
+</pre>`,
+});
+
 Vue.component('warn', {
-		template: `
+	template: `
 			<div class="warn-description">
 				<slot></slot>
 			</div>
